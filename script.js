a6d8f6292c4cb916b0a579b7675124e470db2b67
Harsh Pandey <harshp6421@gmail.com>
2023-06-21 09:35:13 +0530

    Completed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ category = 'technology';
 const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apikey;
 
 // API DOCUMENTATION: "https://gnews.io/docs/v4#top-headlines-endpoint"
-
 let req = new Request(url);
 let i = -1;
 const next = () => {
@@ -11,28 +10,13 @@ const next = () => {
         .then(data => data.json())
         .then(data => {
             i++;
-            document.querySelector(".count").innerText = i + "/" + data.articles.length
-            document.querySelector("#news-image").setAttribute("src", `${data.articles[i % data.articles.length].image}`)
-            document.querySelector("h3").innerText = data.articles[i % data.articles.length].title;
-            document.querySelector("p").innerText = data.articles[i % data.articles.length].content.substring(0, 230) + "...";
-            document.querySelector("#url").setAttribute("href", `${data.articles[i % data.articles.length].url}`)
+            const news_value=Math.floor(Math.random()*data.articles.length)
+            document.querySelector("#news-image").setAttribute("src", `${data.articles[news_value].image}`)
+            document.querySelector("h3").innerText = data.articles[news_value].title;
+            document.querySelector("p").innerText = data.articles[news_value].content.substring(0, 230) + "...";
+            document.querySelector("#url").setAttribute("href", `${data.articles[news_value].url}`)
         })
         .catch(e => console.log(e));
 }
-const previous = () => {
-    fetch(req)
-        .then(data => data.json())
-        .then(data => {
-            i--;
-            if (i < 0) {
-                i = data.articles.length - 1;
-            }
-            document.querySelector(".count").innerText = i + "/" + data.articles.length
-            document.querySelector("#news-image").setAttribute("src", `${data.articles[i % data.articles.length].image}`)
-            document.querySelector("h3").innerText = data.articles[i % data.articles.length].title;
-            document.querySelector("p").innerText = data.articles[i % data.articles.length].content.substring(0, 230) + "...";
-            document.querySelector("#url").setAttribute("href", `${data.articles[i % data.articles.length].url}`)
-        })
-        .catch(e => console.log(e));
-}
+
 next()
