e5a7c3179b282fed50677d3de9049e3baf240302
Harsh Pandey <harshp6421@gmail.com>
2023-06-21 10:15:51 +0530

    Completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-apikey = 'edc0e75c66aced48d06570842c846725';
+apikey = "Your API Key Here";
 category = 'technology';
 const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apikey;
 
